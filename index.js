505678e5f7aedf5d927cfb96e1134ffa73505d81
Ben Adrian Sarmiento <benadriansarmiento@gmail.com>
2019-05-20 22:31:43 +0800

    Try await redis incr

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const http = require('http');
-
+const client = require('redis').createClient(process.env.REDIS_URL);
+const {promisify} = require('util');
+const incrAsync = promisify(client.incr).bind(client);
 const PORT = process.env.PORT || 5000
+
 const resBody = {
   message: 'Hello World'
 };
 
-http.createServer(function (req, res) {
+async function incrFoo() {
+  return await incrAsync('foo');
+}
+
+http.createServer(async function (req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(resBody));
+  res.end(JSON.stringify({
+    message: await incrFoo()
+  }));
 }).listen(PORT, () => console.log(`Listening on ${ PORT }`));
