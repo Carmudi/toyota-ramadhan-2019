6eac55d206d7762a5d4e1c3b6754d0ab29113c79
Ben Adrian Sarmiento <benadriansarmiento@gmail.com>
2019-05-20 22:35:50 +0800

    Get key from query param id

--- index.js
+++ index.js
@@ -4,17 +4,13 @@ const {promisify} = require('util');
 const incrAsync = promisify(client.incr).bind(client);
 const PORT = process.env.PORT || 5000
 
-const resBody = {
-  message: 'Hello World'
-};
-
-async function incrFoo() {
-  return await incrAsync('foo');
+async function incrFoo(key) {
+  return await incrAsync(key);
 }
 
 http.createServer(async function (req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(JSON.stringify({
-    message: await incrFoo()
+    count: await incrFoo(req.query.id || 'foo')
   }));
 }).listen(PORT, () => console.log(`Listening on ${ PORT }`));
