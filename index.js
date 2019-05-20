efdd20bf7772f840fe17cb16a26677034806192d
Ben Adrian Sarmiento <benadriansarmiento@gmail.com>
2019-05-20 22:43:40 +0800

    Use carmudi as default key

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const {promisify} = require('util');
 const incrAsync = promisify(client.incr).bind(client);
 const PORT = process.env.PORT || 5000
 
-async function incrFoo(key) {
+async function incr(key) {
   return await incrAsync(key);
 }
 
 http.createServer(async function (req, res) {
-  let query = url.parse(req.url,true).query;
-  let key = (query && query.id) ? query.id : 'foo';
-  let count = await incrFoo(key);
+  let query = url.parse(req.url, true).query;
+  let key = (query && query.id) ? query.id : 'carmudi';
+  let count = await incr(key);
 
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(JSON.stringify({
